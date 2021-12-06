0debe38d77a6c8ebb434a8ea4b13ceeac3c907f6
garykbrixi <garykbrixi@gmail.com>
2021-12-05 23:38:35 -0500

    check_model

--- deepdive/model_opts/feature_extraction.py
+++ deepdive/model_opts/feature_extraction.py
@@ -11,7 +11,7 @@ import torch.nn as nn
 import torch, torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
-            
+
 def prep_model_for_extraction(model):
     if model.training:
         model = model.eval()
@@ -20,7 +20,7 @@ def prep_model_for_extraction(model):
             model = model.cuda()
 
     return(model)
-
+    
 def convert_relu(parent):
     for child_name, child in parent.named_children():
         if isinstance(child, nn.ReLU):
@@ -31,13 +31,13 @@ def convert_relu(parent):
 def get_image_transforms():
     imagenet_stats = {'mean': [0.485, 0.456, 0.406],
                       'std': [0.229, 0.224, 0.225]}
-    
-    options = {'imagenet': [transforms.Resize((224,224)), 
+
+    options = {'imagenet': [transforms.Resize((224,224)),
                             transforms.ToTensor(),
                             transforms.Normalize(**imagenet_stats)]}
-    
-    return {key:transforms.Compose(value) 
-            for (key, value) in options.items()} 
+
+    return {key:transforms.Compose(value)
+            for (key, value) in options.items()}
 
 # Method 1: Flatten model; extract features by layer
 
@@ -50,7 +50,7 @@ class SaveFeatures():
         self.hook.remove()
     def extract(self):
         return self.out
-    
+
 def get_layer_names(layers):
     layer_names = []
     for layer in layers:
@@ -78,49 +78,49 @@ def get_weights_dtype(model):
 def get_module_name(module, module_list):
     class_name = str(module.__class__).split(".")[-1].split("'")[0]
     class_count = str(sum(class_name in module for module in module_list) + 1)
-    
+
     return '-'.join([class_name, class_count])
 
 def get_inputs_sample(inputs):
     if isinstance(inputs, torch.Tensor):
         input_sample = inputs[:3]
-        
+
     if isinstance(inputs, DataLoader):
         input_sample = next(iter(inputs))[:3]
-        
+
     return input_sample
-    
+
 def get_feature_maps_(model, inputs):
     model = prep_model_for_extraction(model)
-    
+
     def register_hook(module):
         def hook(module, input, output):
             module_name = get_module_name(module, feature_maps)
             feature_maps[module_name] = output
-                    
-        if not isinstance(module, nn.Sequential): 
+
+        if not isinstance(module, nn.Sequential):
             if not isinstance(module, nn.ModuleList):
                 hooks.append(module.register_forward_hook(hook))
-            
+
     feature_maps = OrderedDict()
     hooks = []
-    
+
     model.apply(register_hook)
     with torch.no_grad():
         model(inputs)
 
     for hook in hooks:
         hook.remove()
-        
+
     return(feature_maps)
 
 def remove_duplicate_feature_maps(feature_maps, method = 'hashkey', return_matches = False, use_tqdm = False):
     matches, layer_names = [], list(feature_maps.keys())
-        
+
     if method == 'iterative':
-        
+
         target_iterator = tqdm(range(len(layer_names)), leave = False) if use_tqdm else range(len(layer_names))
-        
+
         for i in target_iterator:
             for j in range(i+1,len(layer_names)):
                 layer1 = feature_maps[layer_names[i]].flatten()
@@ -131,32 +131,32 @@ def remove_duplicate_feature_maps(feature_maps, method = 'hashkey', return_match
 
         deduplicated_feature_maps = {key:value for (key,value) in feature_maps.items()
                                          if key not in matches}
-        
+
     if method == 'hashkey':
-        
+
         target_iterator = tqdm(layer_names, leave = False) if use_tqdm else layer_names
         layer_lengths = [len(tensor.flatten()) for tensor in feature_maps.values()]
         random_tensor = torch.rand(np.array(layer_lengths).max())
-        
+
         tensor_dict = defaultdict(lambda:[])
         for layer_name in target_iterator:
             target_tensor = feature_maps[layer_name].flatten()
             tensor_dot = torch.dot(target_tensor, random_tensor[:len(target_tensor)])
             tensor_hash = np.array(tensor_dot).tobytes()
             tensor_dict[tensor_hash].append(layer_name)
-            
+
         matches = [match for match in list(tensor_dict.values()) if len(match) > 1]
         layers_to_keep = [tensor_dict[tensor_hash][0] for tensor_hash in tensor_dict]
-        
+
         deduplicated_feature_maps = {key:value for (key,value) in feature_maps.items()
                                          if key in layers_to_keep}
-    
+
     if return_matches:
         return(deduplicated_feature_maps, matches)
-    
+
     if not return_matches:
         return(deduplicated_feature_maps)
-    
+
 def check_for_input_axis(feature_map, input_size):
     axis_match = [dim for dim in feature_map.shape if dim == input_size]
     return True if len(axis_match) == 1 else False
@@ -168,7 +168,7 @@ def reset_input_axis(feature_map, input_size):
 def get_feature_maps(model, inputs, layers_to_retain = None, remove_duplicates = True):
     model = prep_model_for_extraction(model)
     enforce_input_shape = True
-    
+
     def register_hook(module):
         def hook(module, input, output):
             def process_output(output, module_name):
@@ -189,23 +189,23 @@ def get_feature_maps(model, inputs, layers_to_retain = None, remove_duplicates =
                             feature_maps[module_name] = outputs
                 if layers_to_retain is not None and module_name not in layers_to_retain:
                     feature_maps[module_name] = None
-                            
+
             module_name = get_module_name(module, feature_maps)
-            
+
             if not any([isinstance(output, type_) for type_ in (tuple,list)]):
                 process_output(output, module_name)
-            
+
             if any([isinstance(output, type_) for type_ in (tuple,list)]):
                 for output_i, output_ in enumerate(output):
                     module_name_ = '-'.join([module_name, str(output_i+1)])
                     process_output(output_, module_name_)
-                    
+
         if (not isinstance(module, nn.Sequential) and not isinstance(module, nn.ModuleList)):
             hooks.append(module.register_forward_hook(hook))
-    
+
     feature_maps = OrderedDict()
     hooks = []
-    
+
     model.apply(convert_relu)
     model.apply(register_hook)
     with torch.no_grad():
@@ -213,49 +213,49 @@ def get_feature_maps(model, inputs, layers_to_retain = None, remove_duplicates =
 
     for hook in hooks:
         hook.remove()
-        
+
     feature_maps = {map:features for (map,features) in feature_maps.items()
                         if features is not None}
-        
+
     if remove_duplicates == True:
         feature_maps = remove_duplicate_feature_maps(feature_maps)
-        
+
     return(feature_maps)
 
 def get_empty_feature_maps(model, inputs = None, input_size=(3,224,224), dataset_size=3,
         layers_to_retain = None, remove_duplicates = True, names_only=False):
 
-    
+
     if inputs is not None:
         inputs = get_inputs_sample(inputs)
-        
+
     if inputs is None:
         inputs = torch.rand(3, *input_size)
-        
+
     if next(model.parameters()).is_cuda:
         inputs = inputs.cuda()
-        
+
     empty_feature_maps = get_feature_maps(model, inputs, layers_to_retain, remove_duplicates)
-    
+
     for map_key in empty_feature_maps:
         empty_feature_maps[map_key] = torch.empty(dataset_size, *empty_feature_maps[map_key].shape[1:])
-        
+
     if names_only == True:
         return list(empty_feature_maps.keys())
-    
+
     if names_only == False:
-        return empty_feature_maps  
-    
-    
+        return empty_feature_maps
+
+
 def get_feature_map_names(model, inputs = None, remove_duplicates = True):
     feature_map_names = get_empty_feature_maps(model, inputs, names_only = True,
                                                 remove_duplicates = remove_duplicates)
-    
+
     return(feature_map_names)
 
 def get_feature_map_count(model, inputs = None, remove_duplicates = True):
     feature_map_names = get_feature_map_names(model, inputs, remove_duplicates)
-    
+
     return(len(feature_map_names))
 
 def get_feature_map_metadata(model, input_size=(3,224,224), remove_duplicates = False):
@@ -265,7 +265,7 @@ def get_feature_map_metadata(model, input_size=(3,224,224), remove_duplicates = 
     inputs = torch.rand(3, *input_size)
     if next(model.parameters()).is_cuda:
         inputs = inputs.cuda()
-    
+
     def register_hook(module):
         def hook(module, input, output):
             def process_output(output, module_name):
@@ -301,37 +301,37 @@ def get_feature_map_metadata(model, input_size=(3,224,224), remove_duplicates = 
                     if isinstance(params, torch.Tensor):
                         params = params.item()
                     metadata[module_name]['parameter_count'] = params
-          
+
             module_name = get_module_name(module, metadata)
-          
+
             if not any([isinstance(output, type_) for type_ in (tuple,list)]):
                 process_output(output, module_name)
-        
+
             if any([isinstance(output, type_) for type_ in (tuple,list)]):
                 for output_i, output_ in enumerate(output):
                     module_name_ = '-'.join([module_name, str(output_i+1)])
                     process_output(output_, module_name_)
-              
+
         if (not isinstance(module, nn.Sequential) and not isinstance(module, nn.ModuleList)):
             hooks.append(module.register_forward_hook(hook))
-    
+
     map_data = OrderedDict()
     metadata = OrderedDict()
     hooks = []
-    
+
     model.apply(register_hook)
     with torch.no_grad():
         model(inputs)
 
     for hook in hooks:
         hook.remove()
-        
+
     if remove_duplicates:
         map_data = remove_duplicate_feature_maps(map_data)
         metadata = {k:v for (k,v) in metadata.items() if k in map_data}
-        
+
     return(metadata)
-        
+
 # Helpers: Dataloaders and functions for facilitating feature extraction
 
 class StimulusSet(Dataset):
@@ -344,35 +344,35 @@ class StimulusSet(Dataset):
         if self.transforms:
             img = self.transforms(img)
         return img
-    
+
     def __len__(self):
         return self.images.shape[0]
 
 class CSV2StimulusSet(Dataset):
     def __init__(self, csv, root_dir, image_transforms=None):
-        
+
         self.root = os.path.expanduser(root_dir)
         self.transforms = image_transforms
-        
+
         if isinstance(csv, pd.DataFrame):
             self.df = csv
         if isinstance(csv, str):
             self.df = pd.read_csv(csv)
-        
+
         self.images = self.df.image_name
 
     def __getitem__(self, index):
         filename = os.path.join(self.root, self.images.iloc[index])
         img = Image.open(filename).convert('RGB')
-        
+
         if self.transforms:
             img = self.transforms(img)
-            
+
         return img
-    
+
     def __len__(self):
         return len(self.images)
-        
+
 class Array2StimulusSet(Dataset):
     def __init__(self, img_array, image_transforms=None):
         self.transforms = image_transforms
@@ -386,10 +386,10 @@ class Array2StimulusSet(Dataset):
         if self.transforms:
             img = self.transforms(img)
         return img
-    
+
     def __len__(self):
         return self.images.shape[0]
-    
+
 def get_feature_map_size(feature_maps, layer=None):
     total_size = 0
     if layer is None:
@@ -399,11 +399,9 @@ def get_feature_map_size(feature_maps, layer=None):
             elif torch.is_tensor(feature_maps[map_key]):
                 total_size += feature_maps[map_key].numpy().nbytes / 1000000
         return total_size
-    
+
     if layer is not None:
         if isinstance(feature_maps, np.ndarray):
             return feature_maps[layer].nbytes / 1000000
         elif torch.is_tensor(feature_maps):
             return feature_maps[layer].nbytes / 1000000
-        
-        
--- deepdive/model_opts/feature_reduction.py
+++ deepdive/model_opts/feature_reduction.py
@@ -15,10 +15,17 @@ from sklearn.decomposition import PCA
 from .feature_extraction import *
 from .model_options import *
 
+def check_model(model_string, model = None):
+    if not isinstance(model_string, str):
+        model = model_string
+    model_options = get_model_options()
+    if model_string not in model_options and model == None:
+        raise ValueError('model_string not available in prepped models. Please supply model object.')
+
 def check_reduction_inputs(feature_maps = None, model_inputs = None):
     if feature_maps == None and model_inputs == None:
         raise ValueError('Neither feature_maps nor model_inputs are defined.')
-        
+
     if model_inputs is not None and not isinstance(model_inputs, (DataLoader, torch.Tensor)):
         raise ValueError('model_inputs not supplied in recognizable format.')
 
@@ -30,10 +37,10 @@ def get_feature_map_filepaths(model_string, feature_map_names, output_dir):
 def recursive_delete_if_empty(path):
     if not os.path.isdir(path):
         return False
-    
+
     recurse_list = [recursive_delete_if_empty(os.path.join(path, filename))
                     for filename in os.listdir(path)]
-    
+
     if all(recurse_list):
         os.rmdir(path)
         return True
@@ -46,14 +53,14 @@ def delete_saved_output(output_filepaths, output_dir = None, remove_empty_output
     if output_dir is not None and remove_empty_output_dir:
         output_dir = output_dir.split('/')[0]
         recursive_delete_if_empty(output_dir)
-        
+
 
 def torch_corrcoef(m):
     #calculate the covariance matrix
     m_exp = torch.mean(m, dim=1)
     x = m - m_exp[:, None]
     cov_m = 1 / (x.size(1) - 1) * x.mm(x.t())
-    
+
     #convert covariance to correlation
     d = torch.diag(cov_m)
     sigma = torch.pow(d, 0.5)
@@ -62,16 +69,16 @@ def torch_corrcoef(m):
     cor_m = torch.clamp(cor_m, -1.0, 1.0)
     return cor_m
 
-def srp_extraction(model_string, model = None, model_inputs=None, feature_maps = None, 
-                   n_projections = None, upsampling = True, eps=0.1, seed = 0, 
+def srp_extraction(model_string, model = None, model_inputs=None, feature_maps = None,
+                   n_projections = None, upsampling = True, eps=0.1, seed = 0,
                    output_dir='./srp_arrays', delete_saved_outputs = True,
                    keep_original_feature_maps = True, verbose = False):
-    
+
     check_model(model_string, model)
     check_reduction_inputs(feature_maps, model_inputs)
-        
+
     device_name = 'CPU' if not torch.cuda.is_available() else torch.cuda.get_device_name()
-    
+
     if n_projections is None:
         if feature_maps is None:
             if isinstance(model_inputs, torch.Tensor):
@@ -81,7 +88,7 @@ def srp_extraction(model_string, model = None, model_inputs=None, feature_maps =
         if feature_maps is not None:
             n_samples = next(iter(feature_maps.values())).shape[0]
         n_projections = johnson_lindenstrauss_min_dim(n_samples, eps=eps)
-    
+
     if verbose:
         print('Computing {} SRPs for {} on {}...'.format(n_projections, model_string, device_name))
 
@@ -89,23 +96,23 @@ def srp_extraction(model_string, model = None, model_inputs=None, feature_maps =
     output_dir_exists = os.path.exists(output_dir)
     if not output_dir_exists:
         os.makedirs(output_dir)
-        
+
     if feature_maps is None:
         if model == None:
             model = get_prepped_model(model_string)
-        
+
         model = prep_model_for_extraction(model)
-        
+
         feature_map_names = get_empty_feature_maps(model, names_only = True)
         output_filepaths = get_feature_map_filepaths(model_string, feature_map_names, output_dir)
-        
+
         if not all([os.path.exists(file) for file in output_filepaths.values()]):
             feature_maps = get_all_feature_maps(model, model_inputs)
-            
+
     if feature_maps is not None:
         feature_map_names = list(feature_maps.keys())
         output_filepaths = get_feature_map_filepaths(model_string, feature_map_names, output_dir)
-        
+
     srp_feature_maps = {}
     for feature_map_name in tqdm(feature_map_names, desc = 'SRP Extraction (Layer)'):
         output_filepath = output_filepaths[feature_map_name]
@@ -115,48 +122,48 @@ def srp_extraction(model_string, model = None, model_inputs=None, feature_maps =
                 srp_feature_maps[feature_map_name] = feature_map
             if feature_map.shape[1] >= n_projections or upsampling:
                 srp = SparseRandomProjection(n_projections, random_state=seed)
-                srp_feature_maps[feature_map_name] = srp.fit_transform(feature_map) 
+                srp_feature_maps[feature_map_name] = srp.fit_transform(feature_map)
             np.save(output_filepath, srp_feature_maps[feature_map_name])
         if os.path.exists(output_filepath):
             srp_feature_maps[feature_map_name] = np.load(output_filepath, allow_pickle=True)
         if not keep_original_feature_maps:
                 feature_maps.pop(feature_map_name)
-                
+
     if delete_saved_outputs:
         delete_saved_output(output_filepaths, output_dir, remove_empty_output_dir = True)
-            
+
     return(srp_feature_maps)
 
 def rdm_extraction(model_string, model = None, model_inputs = None, feature_maps = None,
                    use_torch_corr = False, append_suffix = False,
                    output_dir='./rdm_arrays', delete_saved_outputs = True,
                    keep_original_feature_maps = True, verbose = True):
-    
+
     check_model(model_string, model)
     check_reduction_inputs(feature_maps, model_inputs)
-    
+
     device_name = 'CPU' if not torch.cuda.is_available() else torch.cuda.get_device_name()
-    
+
     if verbose:
         print('Computing RDMS for {} on {}...'.format(model_string, device_name))
-    
+
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    
-    output_file = os.path.join(output_dir, model_string + '_rdms.pkl' 
+
+    output_file = os.path.join(output_dir, model_string + '_rdms.pkl'
                            if append_suffix else model_string + '.pkl')
-    
+
     if os.path.exists(output_file):
         model_rdms = pickle.load(open(output_file,'rb'))
-        
+
     if not os.path.exists(output_file):
         if feature_maps is None:
             if model == None:
                 model = get_prepped_model(model_string)
-             
+
             model = prep_model_for_extraction(model)
             feature_maps = get_all_feature_maps(model, model_inputs, numpy = not use_torch_corr)
-        
+
         model_rdms = {}
         for model_layer in tqdm(feature_maps, leave=False):
             if use_torch_corr:
@@ -171,20 +178,20 @@ def rdm_extraction(model_string, model = None, model_inputs = None, feature_maps
                 feature_maps.pop(feature_map)
         with open(output_file, 'wb') as file:
             pickle.dump(model_rdms, file)
-            
+
     return(model_rdms)
 
-def pca_extraction(model_string, model = None, model_inputs=None, feature_maps = None, 
-                   n_components=None, use_imagenet_pca = True, imagenet_path = None, 
+def pca_extraction(model_string, model = None, model_inputs=None, feature_maps = None,
+                   n_components=None, use_imagenet_pca = True, imagenet_path = None,
                    output_dir='./pca_arrays', delete_saved_outputs = True,
                    keep_original_feature_maps = True, verbose = True):
-    
+
     check_model(model_string, model)
     check_reduction_inputs(feature_maps, model_inputs)
-    
+
     if use_imagenet_pca == True and imagenet_path is None:
         raise ValueError('use_imagenet_pca selected, but imagenet_path not specified.')
-    
+
     if feature_maps is None:
         if isinstance(model_inputs, torch.Tensor):
             n_samples = len(model_inputs)
@@ -192,25 +199,25 @@ def pca_extraction(model_string, model = None, model_inputs=None, feature_maps =
             n_samples = len(model_inputs.dataset)
     if feature_maps is not None:
         n_samples = next(iter(feature_maps.values())).shape[0]
-    
+
     if n_components is not None:
         if n_components > 1000 and use_imagenet_pca:
             raise ValueError('Requesting more components than are available with PCs from imagenet sample.')
-        if n_components > n_samples: 
+        if n_components > n_samples:
             raise ValueError('Requesting more components than are available with stimulus set sample size.')
-            
+
     if n_components is None:
         if use_imagenet_pca:
             n_components = 1000
         if not use_imagenet_pca:
             n_components = n_samples
-    
+
     if verbose:
         device_name = 'CPU' if not torch.cuda.is_available() else torch.cuda.get_device_name()
-    
+
     pca_type = 'imagenet_1000' if use_imagenet_pca else 'stimulus_direct'
-    pca_printout = '1000 ImageNet PCs' if use_imagenet_pca else 'up to {} Stimulus PCs'.format(n_components) 
-    
+    pca_printout = '1000 ImageNet PCs' if use_imagenet_pca else 'up to {} Stimulus PCs'.format(n_components)
+
     print('Computing {} for {} on {}...'.format(pca_printout, model_string, device_name))
 
     output_dir = os.path.join(output_dir, pca_type, model_string)
@@ -219,28 +226,28 @@ def pca_extraction(model_string, model = None, model_inputs=None, feature_maps =
 
     if model == None:
         model = get_prepped_model(model_string)
-        
+
     model = prep_model_for_extraction(model)
-        
+
     if feature_maps is None:
         feature_map_names = get_empty_feature_maps(model, names_only = True)
         output_filepaths = get_feature_map_filepaths(model_string, feature_map_names, output_dir)
-        
+
         if not all([os.path.exists(file) for file in output_filepaths.values()]):
             print('Now extracting feature maps for stimulus set...')
             feature_maps = get_all_feature_maps(model, model_inputs)
-            
+
     if feature_maps is not None:
         feature_map_names = list(feature_maps.keys())
         output_filepaths = get_feature_map_filepaths(model_string, feature_map_names, output_dir)
-        
+
     if not all([os.path.exists(file) for file in output_filepaths.values()]) and use_imagenet_pca:
         imagenet_images, imagenet_transforms = np.load(imagenet_path), get_image_transforms()['imagenet']
         imagenet_loader = DataLoader(Array2DataSet(imagenet_images, imagenet_transforms), batch_size=64)
 
         print('Now extracting feature maps for imagenet_sample...')
         imagenet_feature_maps = get_all_feature_maps(model, imagenet_loader)
-    
+
     print('Computing PCA transforms...')
     pca_feature_maps = {}
     for feature_map_name in tqdm(feature_map_names):
@@ -264,6 +271,5 @@ def pca_extraction(model_string, model = None, model_inputs=None, feature_maps =
             feature_maps.pop(feature_map_name)
             if use_imagenet_pca:
                 imagenet_feature_maps.pop(feature_map_name)
-            
-    return(pca_feature_maps)
 
+    return(pca_feature_maps)
